2fdf0c0436f1d0461f6e999fba7aff4f48deb3d6
Dmitry Savosh <d.savosh@gmail.com>
2024-05-22 18:21:14 +0300

    Initial Commit

---
+++ RunAppleScriptExample/RunAppleScriptExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunAppleScriptExampleApp.swift
+//  RunAppleScriptExample
+//
+//  Created by user on 22.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct RunAppleScriptExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
